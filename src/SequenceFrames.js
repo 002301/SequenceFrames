ab36a31d61897b6517e390a2148bd533efd6db10
airmn <002301@gmail.com>
2016-12-30 17:29:08 +0800

    0.3更新
    添加了播放到某帧的功能
    sf.nextFrameStop(n); //向前播放并停止到某帧
    sf.prevFrameStop(n); //向后播放并停止帧
    sf.currentFrame();//获取当前帧
    
    修改了图片传入方式，改为传入数组。
    function setLogo(){
        var path = "images/bt/";
        var resource = [];
        for (var i = 1; i < 21; i++) {
            resource.push(path+i+'.png');
        };
        lg = new SequenceFrames("geng",resource);
        lg.play();
    }

--- src/SequenceFrames.js
+++ src/SequenceFrames.js
@@ -1,5 +1,5 @@
 /*
- *  SequenceFrames 0.2 
+ *  SequenceFrames 0.3 
  *  播放图片序列帧的js类,借助canvas标签实现序列帧播放。
  *  http://www.airmn.com/SequenceFrames/demo
  *
@@ -27,6 +27,8 @@ function SequenceFrames(id,url,vtotal,complate,end){
 
     function loadImages () { 
         var imageCounter = 0; 
+        total = imageURL.length;
+        loopEnd = total;
         var onLoad = function(err, msg) { 
             if (err) { 
                 console.log(msg); 
@@ -43,7 +45,8 @@ function SequenceFrames(id,url,vtotal,complate,end){
             var img = new Image(); 
             img.onload = function(e) { onLoad(false); }; 
             img.onerror = function(e) { onLoad(true, e);}; 
-            img.src = imageURL+'/'+i+'.jpg';
+            // console.log(imageURL[i]);
+            img.src = imageURL[i];
             images[i] = img; 
         } 
     } 
@@ -53,14 +56,15 @@ function SequenceFrames(id,url,vtotal,complate,end){
     function rendering (index) { 
         if (!loadedImages) 
             return; 
-        var image = images[index]; 
-        var screen_h = myCanvas.height; 
-        var screen_w = myCanvas.width; 
-        var ratio = getScaleRatio({width:image.width, height:image.height}, {width:screen_w, height:screen_h}); 
-        var img_h = image.height * ratio; 
-        var img_w = image.width * ratio; 
-        ctx.drawImage(image, (screen_w - img_w)*0.5, (screen_h - img_h)*0.5, img_w, img_h); 
-
+        if(images[index]!=undefined){
+           var image = images[index]; 
+            var screen_h = myCanvas.height; 
+            var screen_w = myCanvas.width; 
+            var ratio = getScaleRatio({width:image.width, height:image.height}, {width:screen_w, height:screen_h}); 
+            var img_h = image.height * ratio; 
+            var img_w = image.width * ratio; 
+            ctx.drawImage(image, (screen_w - img_w)*0.5, (screen_h - img_h)*0.5, img_w, img_h); 
+        }
     }
 
     function getScaleRatio (obj1,obj2){
@@ -68,31 +72,21 @@ function SequenceFrames(id,url,vtotal,complate,end){
        var hh = obj2.height/obj1.height;
        return (ww>hh)?ww:hh;
     }
-
-
-    function next (){
+    function next(){
         step++;
-        if(step==loopEnd){
-                step = loopStart;
+        //console.log(step);
+        if(step>=loopEnd){
+            step = loopStart;
         }
-        if(step>=total){
-             clearInterval(handler);
-             end()
-            return false;
-        }else{
-             rendering(step)
-        }
-
+        rendering(step);
     }
 
-    function previous () {
+    function prev(){
         step--;
         if(step<loopStart){
             step = loopEnd;
-        }else{
-             rendering(step)
         }
-        console.log(step)
+        rendering(step);
     }
 
     /*
@@ -106,6 +100,7 @@ function SequenceFrames(id,url,vtotal,complate,end){
     }
     //停止
     this.stop = function (){
+        //console.log('end')
         clearInterval(handler);
     }
     //播放
@@ -113,10 +108,13 @@ function SequenceFrames(id,url,vtotal,complate,end){
         _this.stop();
         handler = setInterval(next,interval);
     }
-    //向前播放
+    //下一帧
+    this.next =function(){
+        next();
+    }
+    //前一帧
     this.prev = function(){
-        _this.stop();
-        handler = setInterval(previous,interval);
+        prev();
     }
     //从第n帧开始播放
     this.gotoAndPlay = function(n){
@@ -146,6 +144,40 @@ function SequenceFrames(id,url,vtotal,complate,end){
             _this.setLoop(0,total)
         }
         _this.prev();
+    }
+    //获取当前帧
+    this.currentFrame = function(){
+        return step;
+    }
+    //向后播放并停止到某帧
+    this.nextFrameStop = function(n,nfend){
+        var goNum = n;
+        var nfend = nfend || Function;
+        //console.log(goNum+"    :"+step)
+        if(goNum!=step){
+           _this.next();
+           handler = setTimeout(function(){_this.nextFrameStop(goNum,nfend)},interval); 
+        }else{
+            _this.stop();
+            nfend();
+        }
+    }
+    //向前播放并停止到某帧
+    this.prevFrameStop = function(n,nfend){
+        var goNum = n;
+        var nfend = nfend || Function;
+        if(goNum!=step){
+           _this.prev();
+           handler = setTimeout(function(){_this.prevFrameStop(goNum,nfend)},interval); 
+        }else{
+            _this.stop();
+            nfend();
+        }
+    }
+
+    this.setInterval = function (n){
+        interval =n;
+        //console.log(interval)
     }
     loadImages();
 }
